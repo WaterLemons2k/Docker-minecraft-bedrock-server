f6d9740c207a37f0c044742b1aeb97aaa059a911
WaterLemons2k <62788816+WaterLemons2k@users.noreply.github.com>
2023-05-21 15:31:36 +0800

    fix: let unzip overwrite bedrock-server

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,8 @@ set -e
 
 cd /minecraft
 while [ -e /bedrock-server.zip ]; do
-    unzip -n -q /bedrock-server.zip
+    echo "Start extracting 'bedrock-server.zip'..."
+    unzip -oq /bedrock-server.zip
     rm /bedrock-server.zip
 done
 ./bedrock_server
